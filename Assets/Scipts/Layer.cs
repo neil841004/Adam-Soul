421da15012edd13d6c29e176145ef81ccd1a1d53
Halaa <neil841004@gmail.com>
2019-04-02 01:51:10 +0800

    Fix Layer

---
+++ Assets/Scipts/Layer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layer : MonoBehaviour
+{
+    public float centerDistance = 0;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = (this.transform.position.z - centerDistance) * -0.01f;
+        this.transform.position = new Vector3(transform.position.x, y, transform.position.z);
+    }
+}
