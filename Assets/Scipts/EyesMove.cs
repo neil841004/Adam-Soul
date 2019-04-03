55355718623d35f1f124d30f5a443639e608aa6d
Halaa <neil841004@gmail.com>
2019-04-04 03:34:01 +0800

    Tom eyes

---
+++ Assets/Scipts/EyesMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyesMove : MonoBehaviour
+{
+    Transform target;
+    Vector3 distance;
+    Vector3 oringinPos;
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        oringinPos = gameObject.transform.parent.GetComponent<Transform>().position;
+        distance = (target.position - transform.position).normalized;
+        transform.position = oringinPos + (distance * 0.07f);
+    }
+}
