2e0fc096c3cfadaf503b5033ace6e54e99c4dccf
Halaa <neil841004@gmail.com>
2019-04-03 18:45:53 +0800

    Fake 3d sound

---
+++ Assets/Scipts/Fake3DSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fake3DSound : MonoBehaviour {
+	AudioSource audio;
+	Transform target;
+	public float distance;
+	public float originVolume = 0.3f;
+	public float decrease = 0.005f;
+	// Use this for initialization
+	void Start () {
+		audio = this.GetComponent<AudioSource>();
+		target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance = (transform.position - target.position).sqrMagnitude;
+		audio.volume = originVolume - distance*decrease;
+	}
+}
